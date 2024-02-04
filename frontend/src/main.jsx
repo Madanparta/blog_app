d8095276c90003c516b51563999bcc7a39baa6f0
Madan k <partagowda15@gmail.com>
2024-02-04 14:22:57 +0530

    add complite dark funcanality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store,persistor} from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,7 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
       <Provider store={store}>
         <BrowserRouter>
+
+        <ThemeProvider>
           <App />
+        </ThemeProvider>
+        
         </BrowserRouter>
       </Provider>
       
