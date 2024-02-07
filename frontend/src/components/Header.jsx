326221a6948a296b7216df2ef623e94213417eb3
Madan k <partagowda15@gmail.com>
2024-02-08 00:23:22 +0530

    Profile page UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
             <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture} rounded/>}>
               <Dropdown.Header>
                 <span className='block text-sm'>@{currentUser.username}</span>
-                <span className='block text-sm font-medium truncate'>@{currentUser.email}</span>
+                <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
               </Dropdown.Header>
               <Link to={'/dashboard?tab=profile'}>
                 <Dropdown.Item>Profile</Dropdown.Item>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar'
+import DarshProfile from '../components/DarshProfile'
 
 const Dashboard = () => {
+  const location = useLocation()
+  const [tab,setTab]=useState('')
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if(tabFromUrl){
+      setTab(tabFromUrl) 
+    }
+  },[location.search])
   return (
-    <div>
-      Dashboard
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md-56'>
+      {/* sidebar */}
+      <DashSidebar/>
+      </div>
+      {/* profile .. */}
+      { tab === 'profile' && <DarshProfile/>}
     </div>
   )
 }
